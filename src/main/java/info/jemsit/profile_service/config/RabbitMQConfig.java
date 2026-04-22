67e6777b5c922472769c05a1fe611678a81add5c
jimmy <jimishukurow@gmail.com>
2026-04-22 10:16:11 +0500

    Add RabbitMQ configuration and adjust Docker memory limits

---
+++ src/main/java/info/jemsit/profile_service/config/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package info.jemsit.profile_service.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionNameStrategy;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitTemplate rabbitTemplate (ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public ConnectionNameStrategy connectionNameStrategy() {
+        return connectionFactory -> "profile-service-connection";
+    }
+}
